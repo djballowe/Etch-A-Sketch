651a4618ca0ef9c04dde7242ffa49f704c3881d6
djballowe <djballowe@gmail.com>
2022-05-09 15:15:17 -0700

    adding the js;

--- Content/script.js
+++ Content/script.js
@@ -1,2 +1,13 @@
 const grid = document.querySelector('.theSketch');
 
+function theSketch(size) {
+    if (size === 'small') {
+        
+    }
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        theSketch(button.id);
+    })
+})
