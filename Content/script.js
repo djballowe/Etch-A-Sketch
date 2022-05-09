814e5d6515c9ea5d91323a1757c52db3f1c9c7bb
djballowe <djballowe@gmail.com>
2022-05-09 15:34:22 -0700

    getting buttons to work

--- Content/script.js
+++ Content/script.js
@@ -1,13 +1,19 @@
+const buttons = document.querySelectorAll('button');
 const grid = document.querySelector('.theSketch');
 
 function theSketch(size) {
     if (size === 'small') {
-        
+        grid.classList.add('.theSketch');
+    } else if (size === 'medium') {
+        grid.classList.add('.theSketchMedium');
+    } else if (size === 'large') {
+        grid.classList.add('theSketchLarge');
     }
 }
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         theSketch(button.id);
-    })
-})
+        console.log('test');
+    });
+});
